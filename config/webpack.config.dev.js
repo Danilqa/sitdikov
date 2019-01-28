e4b66a63c8af4cb0cac094c76cc253e0280bf638
Daniil Sitdikov <hello.boriskas@gmail.com>
2019-01-28 19:53:22 +0300

    common: added html to bundle, added build

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'inline-source-map',
     entry: [
-        path.join(__dirname, './src/index.ts')
+        path.join(__dirname, '../src/index.ts')
     ],
     module: {
         rules: [
@@ -19,11 +20,14 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
+        path: path.resolve(__dirname, '../dist')
     },
     devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
+        contentBase: path.resolve(__dirname, '../dist'),
         historyApiFallback: true,
         port: 3000
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './public/index.html' })
+    ]
 };
---
+++ config/webpack.config.prod.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: [
+        path.join(__dirname, '../src/index.ts')
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, '../build')
+    },
+    plugins: [
+        new CleanWebpackPlugin('build', { root: path.resolve(__dirname, '../') }),
+        new HtmlWebpackPlugin({ template: './public/index.html' })
+    ]
+};
