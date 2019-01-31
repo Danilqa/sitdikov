1e21113b9e8a15f6a81229a2f9487a5effcd8677
Daniil Sitdikov <hello.boriskas@gmail.com>
2019-01-31 11:20:35 +0300

    COREUI-79 common: added public folder, added example for start page

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import { PureComponent } from 'react';
+import { Button, ButtonModeType, Init, theme } from 'core-ui-kit';
 
 export class App extends PureComponent {
 
     render() {
-        return <p>Hello Arrival!</p>
+        return (
+            <Init>
+                <Button mode={ButtonModeType.PRIMARY} theme={theme}>Hello, Arrival!</Button>
+            </Init>
+        )
     }
 }
