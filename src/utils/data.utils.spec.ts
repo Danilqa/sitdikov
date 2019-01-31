cb14fb16ad3b5983981176cb4fbadb6a8e9d4ef3
Daniil Sitdikov <hello.boriskas@gmail.com>
2019-01-31 17:00:26 +0300

    COREUI-79 common: added jest

---
+++ src/utils/data.utils.spec.ts
@@ -0,0 +1,8 @@
+import { DataUtils } from './data.utils';
+
+describe('DataUtils', () => {
+
+    it('should get timestamp', () => {
+        expect(DataUtils.getTimestamp('10.12.2019')).toEqual(1570827600000);
+    });
+});
---
+++ src/utils/data.utils.ts
@@ -0,0 +1,7 @@
+// TODO: This is demo example. Please, remove it.
+export class DataUtils {
+
+    static getTimestamp(date: string): number {
+        return new Date(date).getTime();
+    }
+}
