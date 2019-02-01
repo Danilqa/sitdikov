28e75d4512330eaf4e89450ffcf9f429487e958f
Daniil Sitdikov <hello.boriskas@gmail.com>
2019-02-01 10:41:25 +0300

    COREUI-79 common: added tslint

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import { PureComponent } from 'react';
+import { PureComponent, ReactNode } from 'react';
 import { Button, ButtonModeType, Init, theme } from 'core-ui-kit';
 
 export class App extends PureComponent {
 
-    render() {
+    render(): ReactNode {
         return (
             <Init>
                 <Button mode={ButtonModeType.PRIMARY} theme={theme}>Hello, Arrival!</Button>
             </Init>
-        )
+        );
     }
 }
